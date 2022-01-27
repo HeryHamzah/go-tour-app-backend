5a6dbd39ff617e9c6745092e7aca426658a51247
HeryHamzah <hamzahhery23@gmail.com>
2022-01-27 22:51:19 +0800

    Update

--- user_API/get_transactions.php
+++ user_API/get_transactions.php
@@ -5,14 +5,13 @@
  	$response = array();
  	$id_user = $_POST["id_user"];
 
- 	$queryResult = $connect->query("SELECT a.*, b.name as location FROM tbl_destinations a inner join tbl_locations b on a.id_location = b.id_location inner join tbl_favorites c on a.id_destination = c.id_destination where c.id_user ='$id_user'");
+ 	$queryResult = $connect->query("SELECT * FROM tbl_transactions where id_user ='$id_user'");
 
  	$result = array();
 
 While($fetchData=$queryResult->fetch_assoc()){
 	$result[] = $fetchData;
-}
+	}
 
 echo json_encode($result);
- 	}
-
+}
