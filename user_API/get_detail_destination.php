c74811a8bb5b6f859d3765be5e74db19d0c9e285
HeryHamzah <hamzahhery23@gmail.com>
2022-02-11 23:09:49 +0800

    Update

--- user_API/get_detail_destination.php
+++ user_API/get_detail_destination.php
@@ -5,7 +5,7 @@
  	$response = array();
  	$id_destination = $_POST["id_destination"];
 
- 	$queryResult = $connect->query("SELECT a.*, b.name as location FROM tbl_destinations a INNER JOIN tbl_locations b ON a.id_location = b.id_location WHERE id_destination = '$id_destination'");
+ 	$queryResult = $connect->query("SELECT a.*, b.name as location, AVG(c.rating) as rating FROM tbl_destinations a INNER JOIN tbl_locations b ON a.id_location = b.id_location LEFT JOIN tbl_destination_rating c ON a.id_destination = c.id_destination WHERE a.id_destination = '$id_destination'");
 
  	$result = array();
 
--- user_API/get_full_destinations.php
+++ user_API/get_full_destinations.php
@@ -35,3 +35,21 @@ While($fetchData=$queryResult->fetch_assoc()){
 echo json_encode($result);
  	}
 
+//get_detail_destination.php
+<?php 
+ require '../connect.php';
+
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 	$response = array();
+ 	$id_destination = $_POST["id_destination"];
+
+ 	$queryResult = $connect->query("SELECT a.*, b.name as location FROM tbl_destinations a INNER JOIN tbl_locations b ON a.id_location = b.id_location WHERE id_destination = '$id_destination'");
+
+ 	$result = array();
+
+While($fetchData=$queryResult->fetch_assoc()){
+	$result['result'] = $fetchData;
+}
+
+echo json_encode($result);
+ 	}
