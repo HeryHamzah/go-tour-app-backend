e144af1147c3207075ac019e96c45809029e04e8
HeryHamzah <hamzahhery23@gmail.com>
2022-02-11 21:51:19 +0800

    Update

--- user_API/get_full_destinations.php
+++ user_API/get_full_destinations.php
@@ -15,3 +15,23 @@ While($fetchData=$queryResult->fetch_assoc()){
 
 echo json_encode($result);
  	}
+
+//Get_destinations.php
+<?php 
+ require '../connect.php';
+
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 	$response = array();
+ 	$id_location = $_POST["id_location"];
+
+ 	$queryResult = $connect->query("SELECT a.*, b.name as location FROM tbl_destinations a inner join tbl_locations b on a.id_location = b.id_location where a.id_location ='$id_location'");
+
+ 	$result = array();
+
+While($fetchData=$queryResult->fetch_assoc()){
+	$result[] = $fetchData;
+}
+
+echo json_encode($result);
+ 	}
+
