4badbf5fa86dcc447bbb74d5235df366f1033fe1
HeryHamzah <hamzahhery23@gmail.com>
2022-02-14 20:42:41 +0800

    Update

---
+++ user_API/save_tourguide_ticket.php
@@ -0,0 +1,28 @@
+ <?php
+
+require '../connect.php';
+	
+	if ($_SERVER['REQUEST_METHOD']=="POST") {
+		$response = array();
+		$id_user = $_POST['id_user'];
+		$id_tourguide = $_POST['id_tourguide'];
+		$user_name = $_POST['user_name'];
+		$tourguide_name = $_POST['tourguide_name'];
+		$date_time = $_POST['date_time'];
+		$destinations = $_POST['destinations'];
+		$total_price = $_POST['total_price'];
+		$picture_path = $_POST['picture_path'];
+
+
+		$insert = "INSERT INTO tbl_tourguide_tickets (id, id_user, id_tourguide, user_name, tourguide_name, date_time, destinations, total_price, picture_path) VALUES (NULL, '".$id_user."','".$id_tourguide."','".$user_name."','".$tourguide_name."','".$date_time."', '".$destinations."', '".$total_price."','".$picture_path."')";
+			if (mysqli_query($connect, $insert)) {
+				$response['value']=1;
+				$response['message']="Reservasi Tourguide disimpan";
+				echo json_encode($response);
+			} else {
+				$response['value']=0;
+				$response['message']="Reservasi Tourguide gagal disimpan";
+				echo json_encode($response);
+			}
+		}
+?> 
