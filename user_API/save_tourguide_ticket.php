8084a4c7086b32763b1d5cce66054f518f77aaca
HeryHamzah <hamzahhery23@gmail.com>
2022-02-16 15:26:07 +0800

    Update

--- user_API/save_tourguide_ticket.php
+++ user_API/save_tourguide_ticket.php
@@ -4,6 +4,7 @@ require '../connect.php';
 	
 	if ($_SERVER['REQUEST_METHOD']=="POST") {
 		$response = array();
+		$booking_code = $_POST['booking_code'];
 		$id_user = $_POST['id_user'];
 		$id_tourguide = $_POST['id_tourguide'];
 		$user_name = $_POST['user_name'];
@@ -14,7 +15,7 @@ require '../connect.php';
 		$picture_path = $_POST['picture_path'];
 
 
-		$insert = "INSERT INTO tbl_tourguide_tickets (id, id_user, id_tourguide, user_name, tourguide_name, date_time, destinations, total_price, picture_path) VALUES (NULL, '".$id_user."','".$id_tourguide."','".$user_name."','".$tourguide_name."','".$date_time."', '".$destinations."', '".$total_price."','".$picture_path."')";
+		$insert = "INSERT INTO tbl_tourguide_tickets (id, id_user, id_tourguide, user_name, tourguide_name, date_time, destinations, total_price, picture_path) VALUES ('".$booking_code."', '".$id_user."','".$id_tourguide."','".$user_name."','".$tourguide_name."','".$date_time."', '".$destinations."', '".$total_price."','".$picture_path."')";
 			if (mysqli_query($connect, $insert)) {
 				$response['value']=1;
 				$response['message']="Reservasi Tourguide disimpan";
