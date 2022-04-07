998ef10cddd1bc922417c6e0577ab194629770af
HeryHamzah <hamzahhery23@gmail.com>
2022-04-07 20:05:32 +0800

    Update

--- user_API/get_tourguide_tickets.php
+++ user_API/get_tourguide_tickets.php
@@ -1,7 +1,7 @@
 <?php 
 require '../connect.php';
 
-$queryResult = $connect->query("SELECT a.*, b.name AS user_name, b.image AS user_picture, c.name AS tourguide_name, c.image AS tourguide_picture FROM tbl_tourguide_tickets a INNER JOIN tbl_users b ON a.id_user = b.id INNER JOIN tbl_tourguide c ON a.id_tourguide = c.id");
+$queryResult = $connect->query("SELECT a.*, b.name AS user_name, b.image AS user_picture, b.email AS user_email, b.hp AS user_contact, c.name AS tourguide_name, c.image AS tourguide_picture, c.email AS tourguide_email, c.hp as tourguide_contact FROM tbl_tourguide_tickets a INNER JOIN tbl_users b ON a.id_user = b.id INNER JOIN tbl_tourguide c ON a.id_tourguide = c.id");
 
 $result = array();
 
