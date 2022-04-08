fbf077465b79f82760e8ee1fbe9e8874d928b547
HeryHamzah <hamzahhery23@gmail.com>
2022-04-08 09:24:27 +0800

    Update

--- gotour.sql
+++ gotour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 03 Apr 2022 pada 13.49
+-- Waktu pembuatan: 08 Apr 2022 pada 01.22
 -- Versi server: 5.7.33
 -- Versi PHP: 7.4.19
 
@@ -103,32 +103,6 @@ CREATE TABLE `tbl_tickets` (
   `rating` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tbl_tickets`
---
-
-INSERT INTO `tbl_tickets` (`booking_code`, `id_user`, `id_destination`, `time`, `total_ticket`, `total_price`, `comment`, `rating`) VALUES
-('027N514285', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 5, '1645977600000', 1, 40500, 'rammang2 oke', '5.0'),
-('2J47156048', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 2, '1647705600000', 2, 31000, '', '0'),
-('6A59005767', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 6, '1646323200000', 2, 21000, '', '0'),
-('9257159068', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 10, '1645113600000', 1, 30500, 'hutan pinus comment', '3.0'),
-('93QT6Z4480', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 17, '1646928000000', 1, 10500, '', '0'),
-('9KDBWIYWXA', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 3, '1647014400000', 1, 40500, '', '0'),
-('BI7TRY9XH5', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 12, '1646150400000', 1, 10500, '', '0'),
-('FNODRPNQKU', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 2, '1645977600000', 1, 15500, '', '0'),
-('G6B24IV96D', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 1, '1646064000000', 1, 20500, '', '0'),
-('GDJS68DJS', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 1, '356787453423', 2, 40000, '', '5.0'),
-('JNI1MOXNNM', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 8, '1645372800000', 2, 11000, '', '0'),
-('LBH90Y1UWX', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 14, '1646064000000', 1, 15500, '', '0'),
-('MQUIEC3807', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 7, '1646064000000', 1, 500, 'PLTB terkeren', '5.0'),
-('MRRLCQXGOF', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 2, '1646409600000', 1, 15500, '', '0'),
-('OBCCLFKUZ1', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 11, '1646150400000', 1, 15500, '', '0'),
-('VUUSFNFMAD', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 3, '1646064000000', 1, 40500, '', '4'),
-('VUUSFNFMADas', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 3, '1646064000000', 1, 40500, '', '5'),
-('X12195L0MU', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 16, '1644940800000', 1, 25500, '', '4.0'),
-('XZL7TI5D7V', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 13, '1645286400000', 1, 20500, 'comment for celebes canyon', '5.0'),
-('XZL7TI5D7Vsa', 'mVYQijySQqZFeLkPvouCL47daiW2', 13, '1645286400000', 1, 20500, 'celebes canyon mantap parah gengs', '5.0');
-
 -- --------------------------------------------------------
 
 --
@@ -154,19 +128,18 @@ CREATE TABLE `tbl_tourguide` (
 --
 
 INSERT INTO `tbl_tourguide` (`id`, `name`, `email`, `password`, `image`, `hp`, `id_location`, `balance`, `jk`, `cost`, `status`) VALUES
-(1, 'Ayo Ogunseinde', 'ayo-ogunseinde@gmail.com', '123456', 'ayo-ogunseinde.jpg', '083479464999', '1', '0', 'Perempuan', '50000', 'on'),
-(2, 'Ben Parker', 'ben-parker@gmail.com', '', 'ben-parker.jpg', '086342016888', '1', '0', 'Laki-laki', '60000', 'on'),
-(3, 'Jake Nackos', 'jake-nackos@gmail.com', '', 'jake-nackos.jpg', '086342367998', '2', '0', 'Perempuan', '50000', 'on'),
-(4, 'Jeffery Erhunse', 'jeffery-erhunse@gmail.com', '', 'jeffery-erhunse.jpg', '087654321234', '2', '0', 'Perempuan', '35000', 'on'),
-(5, 'Joseph Gonzalez', 'joseph-gonzalez@gmail.com', '', 'joseph-gonzalez.jpg', '083129865700', '3', '0', 'Laki-laki', '60000', 'on'),
-(6, 'Jurica Koletic', 'jurica-koletice@gmail.com', '', 'jurica-koletic.jpg', '087654321433', '3', '0', 'Laki-laki', '100000', 'on'),
-(7, 'Matheus Ferrero', 'matheus-ferrero@gmail.com', '', 'matheus-ferrero.jpg', '083129125711', '4', '0', 'Perempuan', '50000', 'on'),
-(8, 'Stefan Stefancik', 'stefan-stefancik@gmail.com', '', 'stefan-stefancik.jpg', '083129125111', '4', '0', 'Perempuan', '100000', 'on'),
-(9, 'Stephanie Liverani', 'stephanie-liverani@gmail.com', '', 'stephanie-liverani.jpg', '083528166453', '5', '0', 'Perempuan', '45000', 'on'),
-(10, 'Vicky Hladynets', 'vicky-hladynets@gmail.com', '', 'vicky-hladynets.jpg', '083528195555', '5', '0', 'Laki-laki', '80000', 'on'),
-(11, 'Leon Tan', 'leon-tan@gmail.com', '', 'leon-tan.jpg', '083528195431', '6', '0', 'Perempuan', '50000', 'on'),
-(12, 'Cristina', 'christina@gmail.com', '123456', 'christina.jpg', '081234538882', '6', '0', 'Perempuan', '55000', 'on'),
-(13, 'Fixy', 'fixy@gmail.com', '111111', 'scaled_c72674d4-15dc-4619-b990-2ca93e8cc90b6560314569703364896.jpg', '08362538364', '1', '0', 'Laki-laki', '40000', 'on');
+(1, 'Ayo Ogunseinde', 'ayo-ogunseinde@gmail.com', '123456', 'ayo-ogunseinde.jpg', '085340362050', '1', '0', 'Perempuan', '50000', 'on'),
+(2, 'Ben Parker', 'ben-parker@gmail.com', '123456', 'ben-parker.jpg', '086342016888', '1', '0', 'Laki-laki', '60000', 'on'),
+(3, 'Jake Nackos', 'jake-nackos@gmail.com', '123456', 'jake-nackos.jpg', '086342367998', '2', '0', 'Perempuan', '50000', 'on'),
+(4, 'Jeffery Erhunse', 'jeffery-erhunse@gmail.com', '123456', 'jeffery-erhunse.jpg', '087654321234', '2', '0', 'Perempuan', '35000', 'on'),
+(5, 'Joseph Gonzalez', 'joseph-gonzalez@gmail.com', '123456', 'joseph-gonzalez.jpg', '083129865700', '3', '0', 'Laki-laki', '60000', 'on'),
+(6, 'Jurica Koletic', 'jurica-koletice@gmail.com', '123456', 'jurica-koletic.jpg', '087654321433', '3', '0', 'Laki-laki', '100000', 'on'),
+(7, 'Matheus Ferrero', 'matheus-ferrero@gmail.com', '123456', 'matheus-ferrero.jpg', '083129125711', '4', '0', 'Perempuan', '50000', 'on'),
+(8, 'Stefan Stefancik', 'stefan-stefancik@gmail.com', '123456', 'stefan-stefancik.jpg', '083129125111', '4', '0', 'Perempuan', '100000', 'on'),
+(9, 'Stephanie Liverani', 'stephanie-liverani@gmail.com', '123456', 'stephanie-liverani.jpg', '083528166453', '5', '0', 'Perempuan', '45000', 'on'),
+(10, 'Vicky Hladynets', 'vicky-hladynets@gmail.com', '123456', 'vicky-hladynets.jpg', '083528195555', '5', '0', 'Laki-laki', '80000', 'on'),
+(11, 'Leon Tan', 'leon-tan@gmail.com', '123456', 'leon-tan.jpg', '083528195431', '6', '0', 'Perempuan', '50000', 'on'),
+(12, 'Cristina', 'christina@gmail.com', '123456', 'christina.jpg', '081234538882', '6', '0', 'Perempuan', '55000', 'on');
 
 -- --------------------------------------------------------
 
@@ -185,21 +158,6 @@ CREATE TABLE `tbl_tourguide_tickets` (
   `rating` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tbl_tourguide_tickets`
---
-
-INSERT INTO `tbl_tourguide_tickets` (`id`, `id_user`, `id_tourguide`, `date_time`, `destinations`, `total_price`, `comment`, `rating`) VALUES
-('54199T1SQ8', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 9, '1646150400000', 'Puncak Lappa Laona', '60000', 'komen for Stephanie liverani', '5.0'),
-('5C55757374', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 8, '1646323200000', 'Hutan Pinus, Parang Bugisi, Danau Tanralili', '155000', 'Stefan cukup baik', '4.0'),
-('5M00G55964', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 7, '1645113600000', 'Parang Bugisi, Danau Tanralili', '75000', 'tour guide keren', '5'),
-('6', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 1, '1644940800000', 'Pinisi', '70000', '', '5'),
-('7', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 3, '1645027200000', 'Kawasan Karst, Rammang-rammang', '120000', 'comment for Jack nachos', '5.0'),
-('8772811729', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 10, '1646236800000', 'Celebes Canyon', '100000', 'comment for vicky', '5.0'),
-('dsdsdds', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 1, '1645113600000', 'sddsds', '10000', '4 aja ya, Ayo Ogun', '4.0'),
-('RGTGFYYDU0', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 1, '1649088000000', 'Pinisi, Ammatoa, Pantai Bara', '125000', '', '0'),
-('SU05O78QR6', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 5, '1645200000000', 'PLTB Tolo', '60000', '', '3');
-
 -- --------------------------------------------------------
 
 --
@@ -216,58 +174,6 @@ CREATE TABLE `tbl_transactions` (
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tbl_transactions`
---
-
-INSERT INTO `tbl_transactions` (`id_transaction`, `id_user`, `title`, `picture_path`, `amount`, `time`, `description`) VALUES
-('A0957109055', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '50000', '1646904619063', ''),
-('A1563796287', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '200000', '1644995091949', ''),
-('A1614687448', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '100000', '1644925936716', ''),
-('A2147183002', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '50000', '1646904270519', ''),
-('A3709862876', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '50000', '1646904719774', ''),
-('A3719460130', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '50000', '1646904461949', ''),
-('A3988106926', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '200000', '1644924915335', ''),
-('A5346542548', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '50000', '1646907823136', ''),
-('A5417974300', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '200000', '1645174573192', ''),
-('A7125612326', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '50000', '1646904737443', ''),
-('A8073282889', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Top Up', '', '50000', '1646904424327', ''),
-('B0113173086', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Parang Bugisi', 'Parang_Bugisi_1.jpg', '-15500', '1646131670888', 'Malino'),
-('B0559543180', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Bulu Tombolo', 'Bulu_Tombolo_1.jpg', '-21000', '1646017595128', 'Maros'),
-('B0842095269', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Ammatoa', 'Ammatoa_1.jpg', '-40500', '1645975762631', 'Bulukumba'),
-('B0917979075', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Bulu Tombolo', 'Bulu_Tombolo_1.jpg', '-10500', '1644927863165', 'Maros'),
-('B1269916998', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Pantai Bara', 'Pantai_Bara_1.jpg', '-15500', '1646403375632', 'Bulukumba'),
-('B2254310648', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Pinisi', 'Pinisi_1.jpg', '-20500', '1646015789531', 'Bulukumba'),
-('B3562215553', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Puncak Lappa Laona', 'Lappa_Laona_1.jpg', '-31000', '1644924973745', 'Barru'),
-('B3757225612', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Rammang-rammang', 'Rammang-rammang_1.jpg', '-40500', '1644925116670', 'Maros'),
-('B5194154188', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Pantai Bara', 'Pantai_Bara_1.jpg', '-31000', '1647611434546', 'Bulukumba'),
-('B5235689301', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'PLTB Tolo', 'PLTB_Tolo_1.jpg', '-500', '1646017921059', 'Jeneponto'),
-('B5535535096', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Danau Tanralili', 'Danau_Tanralili_1.jpg', '-10500', '1644925954803', 'Malino'),
-('B6147780994', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Sungai Palattae', 'Sungai_Palattae_1.jpg', '-10500', '1646910188862', 'Pangkep'),
-('B6725468001', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Celebes Canyon', 'Celebes_Canyon_1.jpg', '-20500', '1645174461704', 'Barru'),
-('B7296448527', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Batu Siping', 'Batu_Siping_1.jpg', '-11000', '1645174526323', 'Jeneponto'),
-('B7861799882', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Kalibbong Alloa', 'Kalibbong_Alloa_1.jpg', '-25500', '1644928419153', 'Pangkep'),
-('B8498926790', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Danau Tanralili', 'Danau_Tanralili_1.jpg', '-10500', '1645976100432', 'Malino'),
-('B8665024036', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Ammatoa', 'Ammatoa_1.jpg', '-40500', '1646910203772', 'Bulukumba'),
-('B8923717992', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Puncak Lappa Laona', 'Lappa_Laona_1.jpg', '-15500', '1646018062379', 'Barru'),
-('B9120440034', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Rammang-rammang', 'Rammang-rammang_1.jpg', '-40500', '1645883473252', 'Maros'),
-('B9212626543', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Pantai Bara', 'Pantai_Bara_1.jpg', '-15500', '1645965087318', 'Bulukumba'),
-('B9436835124', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Kawasan Karst', 'Karst_1.jpg', '-30500', '1646130970230', 'Maros'),
-('B9955311240', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Hutan Pinus', 'Hutan_Pinus_1.jpg', '-30500', '1645086564509', 'Malino'),
-('C1286388222', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Ayo Ogunseinde', 'ayo-ogunseinde.jpg', '-125000', '1648979134486', 'Pinisi, Ammatoa, Pantai Bara'),
-('C1483399285', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Vicky Hladynets', 'vicky-hladynets.jpg', '-85000', '1646023917032', 'Bukit Lakeppo'),
-('C1860358245', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Matheus Ferrero', 'matheus-ferrero.jpg', '-75000', '1644995141218', 'Parang Bugisi, Danau Tanralili'),
-('C2642141707', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Jurica Koletic', 'jurica-koletic.jpg', '-115000', '1646133776156', 'Batu Siping, PLTB Tolo, Bukit Bossolo'),
-('C3356971849', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Jake Nackos', 'jake-nackos.jpg', '-120000', '1644925045489', 'Kawasan Karst, Rammang-rammang'),
-('C5738379262', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Stefan Stefancik', 'stefan-stefancik.jpg', '-125000', '1646135168657', 'Parang Bugisi, Danau Tanralili'),
-('C6992733196', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Vicky Hladynets', 'vicky-hladynets.jpg', '-100000', '1646136452827', 'Celebes Canyon'),
-('C7207431273', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Stephanie Liverani', 'stephanie-liverani.jpg', '-65000', '1646023786051', 'Celebes Canyon'),
-('C7228324803', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Joseph Gonzalez', 'joseph-gonzalez.jpg', '-60000', '1644994631014', 'PLTB Tolo'),
-('C7229264137', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Stephanie Liverani', 'stephanie-liverani.jpg', '-60000', '1646135855601', 'Puncak Lappa Laona'),
-('C7344509454', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Joseph Gonzalez', 'joseph-gonzalez.jpg', '-60000', '1645173174407', 'PLTB Tolo'),
-('C7931923280', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Stefan Stefancik', 'stefan-stefancik.jpg', '-155000', '1646212270931', 'Hutan Pinus, Parang Bugisi, Danau Tanralili'),
-('C9487226956', 'V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'Stephanie Liverani', 'stephanie-liverani.jpg', '-65000', '1646023858405', 'Celebes Canyon');
-
 -- --------------------------------------------------------
 
 --
@@ -283,15 +189,6 @@ CREATE TABLE `tbl_users` (
   `image` text NOT NULL,
   `balance` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `tbl_users`
---
-
-INSERT INTO `tbl_users` (`id`, `email`, `name`, `hp`, `register_date`, `image`, `balance`) VALUES
-('mVYQijySQqZFeLkPvouCL47daiW2', 'scarlet@gmail.com', 'Scrlet', '', '2022-03-02 17:48:22', 'scaled_IMG_20220206_204619.jpg', '0'),
-('nqaSL6uvIvdyx0zIZXQhCh2l6YC3', 'scarlet.indonesia27@gmail.com', 'User Pro', '', '2022-03-02 22:11:56', '', '0'),
-('V7sf6pkHiEduQ6jaKyXtHHxkfr73', 'hery@gmail.com', 'Hery Hz', '085340365886', '2022-01-27 19:02:15', 'scaled_IMG_20220309_013507_628.jpg', '282500');
 
 --
 -- Indexes for dumped tables
